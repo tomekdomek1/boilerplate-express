092a780fad0c6d6d4bf5b51768902f5ecfb225ad
Tomasz Buklaho <flyff2234@gmail.com>
2022-11-25 16:07:13 +0100

    five challenges

--- myApp.js
+++ myApp.js
@@ -1,13 +1,28 @@
 let express = require('express');
 let app = express();
 
+/*
 console.log("Hello World");
+*/ //first challenge
 
+/*
+app.get("/", function (req, res){
+    res.send("Hello Express");
+});
+*/ //second challenge
 
+app.get("/", function (req, res){
+    res.sendFile(__dirname + "/views/index.html");
+})
+//third challenge
 
+app.use("/public", express.static(__dirname + "/public"));
+//forth
 
-
-
+app.get("/json", function(req, res){
+    res.json({"message": "Hello json"});
+})
+//fifth
 
 
 
